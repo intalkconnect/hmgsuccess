2cd90ceba95105ec87ad0d269ad1fb66a58b4cea
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-29 14:59:20 -0300

    Create uploadMediaToWhatsapp.js

---
+++ services/wa/uploadMediaToWhatsapp.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import FormData from 'form-data';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export async function uploadMediaToWhatsapp(fileUrl, type = 'image') {
+  const form = new FormData();
+  form.append('messaging_product', 'whatsapp');
+  form.append('type', type);
+  form.append('file', await axios.get(fileUrl, { responseType: 'stream' }).then(res => res.data));
+
+  const res = await axios.post(
+    `https://graph.facebook.com/v17.0/${process.env.PHONE_NUMBER_ID}/media`,
+    form,
+    {
+      headers: {
+        ...form.getHeaders(),
+        Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+      },
+    }
+  );
+
+  return res.data.id;
+}
