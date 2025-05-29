2b8b0de1e91c9b106fa7c4c37924ea5e51704100
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-28 21:21:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import Fastify from 'fastify';
 import dotenv from 'dotenv';
 import webhookRoutes from './routes/webhook.js';
 import messageRoutes from './routes/messages.js';
+import flowRoutes from './routes/flow.js';
 import { initDB } from './services/db.js';
 
 dotenv.config();
@@ -13,6 +14,7 @@ await initDB();
 
 fastify.register(webhookRoutes, { prefix: '/webhook' });
 fastify.register(messageRoutes, { prefix: '/messages' });
+fastify.register(flowRoutes, { prefix: '/flow' });
 
 fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' }, (err, address) => {
   if (err) throw err;
