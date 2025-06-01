6bae1245a664eb35269107eb120aaf63aba68761
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-31 22:02:19 -0300

    Delete chatbot/index.js

--- chatbot/index.js
+++
@@ -1,9 +0,0 @@
-// engine/index.js
-import { processFlow } from './flowExecutor.js';
-import { loadSession, saveSession } from './sessionManager.js';
-
-export async function processMessage(message, flow, vars, rawUserId) {
-  const userId = `${rawUserId}@c.wa.msginb.net`;
-  const session = await loadSession(userId);
-  return processFlow(message, flow, vars, session, userId);
-}
