cfe5b32dc2fb94a3bd3bb0c47b0f9ffc9c118731
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-06-02 00:49:57 -0300

    Update messenger.js

--- chatbot/messenger.js
+++ chatbot/messenger.js
@@ -1,6 +1,6 @@
 // engine/messenger.js
 import { sendWebchatMessage } from '../services/sendWebchatMessage.js';
-import { sendWhatsappMessage } from '../services/sendWhatsappMessage.js';
+import { sendWhatsappMessage, markAsReadAndTyping } from '../services/sendWhatsappMessage.js';
 
 /**
  * Encapsula envio em diferentes canais.
@@ -18,4 +18,11 @@ export async function sendMessageByChannel(channel, to, type, content) {
   return sendWhatsappMessage({ to, type, content: whatsappContent });
 }
 
-
+/**
+ * Marca a mensagem como lida e exibe indicador de digitação, se houver ID.
+ */
+export async function markAsReadIfNeeded(message) {
+  if (message?.id) {
+    await markAsReadAndTyping(message.id);
+  }
+}
